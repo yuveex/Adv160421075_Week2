e6444cb8dc42cd31cdd538e6dc8df8d1905a3eb4
yuveex <alvinsetiawan2003@gmail.com>
2024-03-06 20:45:15 +0700

    Navigation Class

--- Adv160421075Week2/app/src/main/java/com/ubaya/adv160421075week2/MainFragment.kt
+++ Adv160421075Week2/app/src/main/java/com/ubaya/adv160421075week2/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ubaya.adv160421075week2.databinding.FragmentMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,12 +19,26 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+
+//        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonStart.setOnClickListener{
+            val playerName = binding.txtName.text.toString()
+            val action = MainFragmentDirections.actionMainFragmentToGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
